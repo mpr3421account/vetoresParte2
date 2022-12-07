4e9df67c346147e1eb93bd4b01ea9d16a1358630
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 11:19:29 -0300

    Problema exemplo 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using vetoresParte2;
+using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+Product[] prodvec = new Product[n];
+
+for (int i = 0; i< n; i++)
+{
+    string name = Console.ReadLine();
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    prodvec[i] = new Product(name, price);
+}
+
+double sum = 0.0;
+for (int i = 0; i< n; i++)
+{
+    sum += prodvec[i].Price;
+}
+
+double avg = sum/ n;
+
+Console.WriteLine("AVERAGE PRICE = " + avg);
